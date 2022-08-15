be67e6287fced8a78a0f1bd03095cf1b26948560
MariyaS05 <mariya_govorushko@mail.ru>
2022-08-15 21:15:24 +0300

    Add project

---
+++ 12HwMariya/ThirdViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ThirdViewController.swift
+//  12HwMariya
+//
+//  Created by Мария  on 13.08.22.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        let tagButtons = sender.tag
+        result.text = result.text! + String(tagButtons)
+        acButton.setTitle("C", for: .normal)
+        
+        
+    }
+    @IBAction func buttonsAny(_ sender: UIButton) {
+        let tagButtons = (sender as! UIButton).tag
+        if tagButtons == 10 {
+            result.text = ""
+            acButton.setTitle("AC", for: .normal)
+        }
+       
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+   
+}
---
+++ 12HwMariya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12HwMariya
+//
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
